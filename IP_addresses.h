ab04f5b821e0f3249bb3fe895441bfd3ca2291b4
Sergei.Shibanov <sergei.shibanov@aspose.com>
2024-01-17 11:20:56 +0500

    Revert "remove all files for review"
    This reverts commit 500ead3055650f00e7a0c220672215f64777f279.

---
+++ IP_addresses.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include <vector>
+#include <iostream>
+
+#include "IP_addr.h"
+
+class IP_addresses
+{
+public:	
+	bool ReadData(std::string fileName);
+	void ReadDataFromCin();
+	void PrintAll();
+	void PrintSelected(bool (*predicat)(IP_addr));
+	void BubleSort(); // Since there was no lecture on algorithms yet, simple custom sorting was added.
+
+private:
+	std::vector<IP_addr> addresses;
+
+	// The better return IP_addr, but by the topic of the lesson made with using tuple.
+	std::tuple<uint8_t, uint8_t, uint8_t, uint8_t> StrToIPParts(const std::string& str);
+	void TryDecodeAndThenAdd(const std::string& line);
+};
